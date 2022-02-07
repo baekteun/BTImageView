c75459911b5081bf08e53c6b2d8b48c25dfd2279
baegteun <baegteun@gmail.com>
2022-02-08 01:58:21 +0900

    :test_tube: :: 3 test

--- Tests/BTImageViewTests/BTImageViewTests.swift
+++ Tests/BTImageViewTests/BTImageViewTests.swift
@@ -2,10 +2,30 @@ import XCTest
 @testable import BTImageView
 
 final class BTImageViewTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual("Hello, World!", "Hello, World!")
+    
+    func textInit_createBTImageViewClass() {
+        XCTAssertNotNil(BTImageView())
     }
+    
+    func test_Init_propertyChange() {
+        let align = [2,3]
+        let spacing: CGFloat = 8
+        let axis = NSLayoutConstraint.Axis.vertical
+        
+        let btv = BTImageView(aligns: align, spacing: spacing, axis: axis)
+        XCTAssertEqual(btv.aligns, align)
+        XCTAssertEqual(btv.spacing, spacing)
+        XCTAssertEqual(btv.axis, axis)
+    }
+    
+    func test_addImage() {
+        let image = UIImage()
+        
+        let iv = BTImageView()
+        iv.addImage(image: image)
+        
+        XCTAssertEqual(iv.images.count, 1)
+    }
+    
+    
 }
