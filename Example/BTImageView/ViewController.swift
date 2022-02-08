462a943fc018ccddf28e58a51c1ae5ee750e86df
baegteun <baegteun@gmail.com>
2022-02-08 14:44:59 +0900

    :sparkles: :: Example

--- Example/BTImageView/ViewController.swift
+++ Example/BTImageView/ViewController.swift
@@ -7,15 +7,26 @@
 //
 
 import UIKit
+import BTImageView
 
 class ViewController: UIViewController {
     
-    
-    
-    
+    @IBOutlet weak var iv: BTImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        iv.aligns = [2,3,2]
+        iv.axis = .vertical
+        iv.setImages(images: [
+            .init(systemName: "1.circle") ?? .init(),
+            .init(systemName: "2.circle") ?? .init(),
+            .init(systemName: "3.circle") ?? .init(),
+            .init(systemName: "4.circle") ?? .init(),
+            .init(systemName: "5.circle") ?? .init(),
+            .init(systemName: "6.circle") ?? .init(),
+            .init(systemName: "7.circle") ?? .init(),
+            .init(systemName: "8.circle") ?? .init(),
+            .init(systemName: "9.circle") ?? .init(),
+        ])
     }
 
     override func didReceiveMemoryWarning() {
